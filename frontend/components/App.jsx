d5060315edf03743d3934205936a5ccd31d135e2
Jdarmoni <jgdarmoni@gmail.com>
2019-03-27 14:04:14 -0400

    front end auth and stylings

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import GreetingContainer from '../components/greeting/greeting_container'
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
-import {Route} from 'react-router-dom'
+import {Route, Switch, Link} from 'react-router-dom'
+import { AuthRoute } from '../util/route_util';
 
 const App = ()=>{
     return (
     <div>
         <header>
+            <Link to="/" className="header-link">
         <h1>GoodLivres</h1>
+            </Link>
         <GreetingContainer />
         </header>
 
-        <Route path="/login" component={LoginFormContainer}/>
-        <Route path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
     </div>
 )}
--- frontend/goodLivres.jsx
+++ frontend/goodLivres.jsx
@@ -6,7 +6,19 @@ import {signup, login, logout} from './actions/session_actions'
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     // ********TESTING START
     window.getState = store.getState;
     window.dispatch = store.dispatch;

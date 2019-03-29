4bcf6419c82775f2ddc58172a8ef104cf46417d0
Jdarmoni <jgdarmoni@gmail.com>
2019-03-29 15:17:13 -0400

    heroku push

--- frontend/components/bookshelf/bookshelf_index.jsx
+++ frontend/components/bookshelf/bookshelf_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import BookshelfIndexItem from './bookshelf_index_item'
+import BookshelfIndexItem from './bookshelf_index_eggs'
 
 class BookshelfIndex extends React.Component{
     

72eace3e424b9fda89c88623cbc3d9cabc68ce91
Jdarmoni <jgdarmoni@gmail.com>
2019-05-18 14:55:53 -0400

    restyle logout button in header

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -104,7 +104,7 @@ class Greeting extends React.Component{
 
                 <hgroup className="header-group">
                     {/* <img className ="header-image" src ="https://secure.gravatar.com/avatar/ee9daacbf73553c82285907caadd3d8a?secure=true&size=300"/>  */}
-                    <button className="header-button" onClick={this.logOut}>Log Out</button>
+                    <button className="header-button" onClick={this.logOut}>Logout</button>
                 </hgroup>
                 </div>
             </nav>
